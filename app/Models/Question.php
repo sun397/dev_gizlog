9f541064f86e894236a6fa399ebac4e71d9eb082
尾形朝日 <>
2019-07-01 19:35:54 +0900

    インデント修正_

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -40,16 +40,16 @@ class Question extends Model
 
     public function category()
     {
-		    return $this->belongsTo(TagCategory::class, 'tag_category_id');
+        return $this->belongsTo(TagCategory::class, 'tag_category_id');
     }
 
     public function comment()
     {
-		    return $this->hasMany(Comment::class);
+        return $this->hasMany(Comment::class);
     }
 
     public function user()
     {
-		    return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class);
     }
 }
